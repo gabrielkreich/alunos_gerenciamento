b58a66523a811490480618608019ab2f8ba2856a
gabrielkreich <gabriel.kreich@alunos.sc.senac.br>
2025-05-20 18:51:11 -0300

    primeiro commit

---
+++ APP/logica_sistemas.py
@@ -0,0 +1,69 @@
+from alunos_gerenciamento.models.aluno import Aluno
+from alunos_gerenciamento.models.curso import Curso
+
+CURSOS = {}     #simulação de banco de dados
+ALUNOS = {}     #simulação de banco de dados
+
+def cadastrar_aluno(nome, nascimento, nome_curso=None):       #não precisa de SELF pois está fora de uma classe
+    if not nome or not nascimento:
+        return "Parametros inválidos"
+
+    c = None
+    aluno_objeto = Aluno(nome, nascimento)
+
+    if nome_curso:
+        c = CURSOS.get(nome_curso)
+        c.alunos[aluno_objeto.matricula] = aluno_objeto
+
+    ALUNOS[aluno_objeto.matricula] = aluno_objeto
+
+    return {
+        "nome_aluno": aluno_objeto.nome,
+        "matricula": aluno_objeto.matricula,
+        "curso": c.nome or None
+    }
+
+def listar_alunos():
+    resposta = ""
+    for aluno in ALUNOS.values():
+        resposta += (f"Nome: {aluno.nome} \n"
+                     f"Matricula: {aluno.matricula} \n"
+                     f"Curso: {aluno.curso.nome} \n"
+                     f"------------------------ \n")
+
+    return resposta
+
+def detalhar_aluno(matricula):
+    if not matricula:
+        return "Paramentros inválidos"
+
+    aluno = ALUNOS.get(matricula)
+
+    if not aluno:
+        return "Este aluno não está cadastrado."
+
+    return (f"Nome: {aluno.nome} \n"
+            f"Matricula: {aluno.matricula} \n"
+            f"Data de nascimento: {aluno.nascimento} \n"
+            f"Data de ingresso: {aluno.ingresso} \n"
+            f"Curso: {aluno.curso.nome or "Sem curso no momento"} \n"
+            f"Notas: {aluno.notas}")
+
+def deletar_aluno(matricula):
+    if not matricula:
+        return "Paramentros inválidos"
+
+    aluno = ALUNOS.get(matricula)
+
+    if not aluno:
+        return "Este aluno não está cadastrado."
+
+    if aluno.curso:
+
+        curso = CURSOS.get(aluno.curso.nome)
+        curso.alunos.pop(matricula)
+
+    ALUNOS.pop(matricula)               #.pop para excluir
+
+    return "Aluno excluido com sucesso!"
+
---
+++ main.py
@@ -0,0 +1,40 @@
+from alunos_gerenciamento.APP.logica_sistemas import cadastrar_aluno, listar_alunos, detalhar_aluno, deletar_aluno
+
+
+
+comando = ""
+
+
+
+while comando != "sair":
+    comando = input(f"Escolha uma opção: \n"
+                    f"1) Cadastrar Aluno \n"
+                    f"2) Listar Alunos \n"
+                    f"3) Detalhar Aluno \n"
+                    f"4) Deletar Aluno \n"
+                    f"Digite 'sair' para sair do sistema \n")
+
+    match comando:
+        case "1":
+            nome = input("Informe o nome do aluno: ")
+            nascimento = input("informe a data de nascimento do aluno: ")
+            curso = input("Informe o curso do aluno se tiver, se não deixe vazio: ")
+
+            print(cadastrar_aluno(nome, nascimento, curso))
+
+        case "2":
+            print(listar_alunos())
+
+        case "3":
+            matricula = input("Informe a matricula do aluno: ")
+
+            print(detalhar_aluno(matricula))
+
+        case "4":
+            matricula = input("informe a matricula do aluno: ")
+
+            print(deletar_aluno(matricula))
+
+        case "sair":
+            print("Saindo do sistema.")
+
